0ee6d88238402a6915afee0a758c131d4038a230
BryceVE <0362729@schoolsnet.act.edu.au>
2022-08-03 13:22:29 +1000

    contact us form working

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(Config)  # loads the configuration for the database
+db = SQLAlchemy(app)  # creates the db object using the configuration
+
+from models import Contact
+from forms import ContactForm
 
 
 @app.route('/')
@@ -12,5 +17,13 @@ def homepage():  # put application's code here
 
 if __name__ == '__main__':
     app.run()
-    app.config.from_object(Config)  # loads the configuration for the database
-    db = SQLAlchemy(app)  # creates the db object using the configuration
+
+
+@app.route("/contact.html", methods=["POST", "GET"])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        new_contact = Contact(name=form.name.data, email=form.email.data, message=form.message.data)
+        db.session.add(new_contact)
+        db.session.commit()
+    return render_template("contact.html", title="Contact Us", form=form)
